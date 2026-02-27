11214e6196fb8f078976954d1d011396e9d36a9a
ItaloStcDev <italostcdev@gmail.com>
2026-02-27 04:55:06 -0300

    Fix message formatting for betting result

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function apostar() {
         return divRes.innerHTML = `<p>Por favor, insira um número valido.</p>`;
     }
 
-    divRes.innerHTML = `<p>Você apostou: R$${valorFixado} <strong>perdeu tudo e</strong> <div id="cft">FOI JOGAR NO VASCO</div></p><br><img src="https://www.ogol.com.br/img/logos/equipas/2258_imgbank_1683640645.png" alt="vasco"><h2 id="cft"><strong>BEM VINDO AO GIGANTE!!</strong></h2>`;
+    divRes.innerHTML = `<p>Você apostou: R$${valorFixado} <strong>perdeu tudo e</strong> <div id="cft">FOI JOGAR NO VASCO!</div></p><br><img src="https://www.ogol.com.br/img/logos/equipas/2258_imgbank_1683640645.png" alt="vasco"><h2 id="cft"><strong>BEM VINDO AO GIGANTE!!</strong></h2>`;
 
     // const result = document.createElement("div").setAttribute()
     // divApostando.appendChild();
